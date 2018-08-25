701278cf96dcba1b0af5b75a5b2193398111092d
Paul Duvall <duvallpj@gmail.com>
2018-08-25 09:53:36 +0100

    Slight correction to algorithm 4 so that correct variable is returned.

--- 04. largest_number_from_subarrays/largest_number_from_subarrays.js
+++ 04. largest_number_from_subarrays/largest_number_from_subarrays.js
@@ -13,8 +13,8 @@ function largestOfFour(arr) {
       }
     }
   }
-  console.log(numberTracker);
-  return arr;
+  
+  return numberTracker;
 }
 
 largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]);
