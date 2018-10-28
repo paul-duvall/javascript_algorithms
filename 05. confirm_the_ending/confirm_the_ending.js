7a7c57fdee6baf3bdffa9a938b1b6be710e1f808
Paul Duvall <duvallpj@gmail.com>
2018-10-28 13:48:37 +0000

    Update to algorithm 5 - confirm the ending of a string

--- 05. confirm_the_ending/confirm_the_ending.js
+++ 05. confirm_the_ending/confirm_the_ending.js
@@ -1,46 +1,21 @@
 // Checks whether a string ends with a given target string
-// Version 1 logs the boolean result in a variable...
 
 function confirmEnding(str, target) {
-  
+
   // The position of the first letter checked
-  startNumber = str.length - target.length;
+  let startNumber = str.length - target.length;
   // The position of the last letter checked
-  endNumber = str.length;
+  let endNumber = str.length;
   // The section of str being checked against
-  endString = str.substring(startNumber,endNumber);
+  let endString = str.substring(startNumber,endNumber);
 
   if(endString === target) {
-    result = true;
-  } else {
-    result = false;
-  }
+    return true;
+  } 
 
-  console.log(result);
-  return result;
+  return false;
 }
 
-confirmEnding("congratulation", "on");
+let result = confirmEnding("Bastian", "iane");
 
-// Version 2 works with code camp and just results 'true' or 'false (commented out)
-
-// Checks whether a string ends with a given target string
-
-// function confirmEnding(str, target) {
-
-//   let result;
-//   // The position of the first letter checked
-//   let startNumber = str.length - target.length;
-//   // The position of the last letter checked
-//   let endNumber = str.length;
-//   // The section of str being checked against
-//   let endString = str.substring(startNumber,endNumber);
-
-//   if(endString === target) {
-//     return true;
-//   } 
-
-//   return false;
-// }
-
-// confirmEnding("Bastian", "toon");
+console.log(result);
